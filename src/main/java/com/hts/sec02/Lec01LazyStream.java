c1736361a029346c71df46c4bf979e7b89a623aa
Moses Hunsu <hunsumoses7@gmail.com>
2025-01-28 16:28:33 +0100

    looked at lazy stream

---
+++ src/main/java/com/hts/sec02/Lec01LazyStream.java
@@ -0,0 +1,20 @@
+package com.hts.sec02;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.stream.Stream;
+
+public class Lec01LazyStream {
+
+    public static final Logger log = LoggerFactory.getLogger(Lec01LazyStream.class);
+
+    public static void main(String[] args) {
+
+        Stream.of(1)
+                .peek(i -> log.info("received : {}", i))
+                .toList();
+
+    }
+
+}
